8dc97a1746d36e2fb9974567c68fea6c381331f3
Théophane Hufschmitt <theophane.hufschmitt@tweag.io>
2023-11-30 14:24:06 +0100

    Flatten the Nix shells
    Only use one Nix shell which provides everything we need, and wrap
    `stack` to not try to call Nix at all.
    
    The downside of this is that we have to manually keep the GHC version in
    sync with the one Stack expects, but it also makes things quite simpler.

--- shell.nix
+++ shell.nix
@@ -2,11 +2,8 @@ let
   sources = import ./nix/sources.nix;
   pkgs = import sources.nixpkgs { };
 
-  # Wrap Stack to configure Nix integration and target the correct Stack-Nix file
-  #
-  # - nix: Enable Nix support
-  # - no-nix-pure: Pass environment variables, like `NIX_PATH`
-  # - nix-shell-file: Specify the Nix file to use (otherwise it uses `shell.nix` by default)
+  # Since we're already in a Nix shell, we need to wrap `stack` so that it
+  # doesn't try to use Nix at all (but uses the GHC we provide in the shell)
   stack-wrapped = pkgs.symlinkJoin {
     name = "stack";
     paths = [ pkgs.stack ];
@@ -14,20 +11,21 @@ let
     postBuild = ''
       wrapProgram $out/bin/stack \
         --add-flags "\
-          --nix \
-          --no-nix-pure \
-          --nix-shell-file=nix/stack-integration.nix \
+          --system-ghc \
+          --no-nix \
         "
     '';
   };
 
 in
 pkgs.mkShell {
-  buildInputs = [
+  nativeBuildInputs = [
+    # Needs to be kept in sync with the one that the current stack snapshot uses
+    pkgs.haskell.compiler.ghc8107
     stack-wrapped
   ];
 
-  # Configure the Nix path to our own `pkgs`, to ensure Stack-with-Nix uses the correct one rather than the global <nixpkgs> when looking for the right `ghc` argument to pass in `nix/stack-integration.nix`
-  # See https://nixos.org/nixos/nix-pills/nix-search-paths.html for more information
-  NIX_PATH = "nixpkgs=" + pkgs.path;
+  buildInputs = [
+    pkgs.zlib
+  ];
 }
